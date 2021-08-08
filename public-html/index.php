65126ebe59030c24bff61eb27714d05c2e930534
haolam05 <tuonghao2001@gmail.com>
2021-08-07 22:46:36 -0700

    Bootstrap client-side form validation

--- public-html/index.php
+++ public-html/index.php
@@ -53,8 +53,7 @@
                                         ?>" 
                                     id="formSignUpEmail" 
                                     placeholder="Enter your email address" 
-                                    required 
-                                    pattern="^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$" 
+                                    onkeyup="jsSignUpValidateEmail()"
                                     name="formSignUpEmail">
                                     <?php if ($_SESSION['msgid'] == '801') { ?>
                                             <div class="invalid-feedback">
@@ -68,27 +67,28 @@
                                     class="form-control <?php echo (phpShowInputFeedback($_SESSION['msgid'])[0]); ?>"
                                     id="formSignUpPassword" 
                                     placeholder="Enter your password" 
-                                    required 
-                                    pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$#]).{8,16}" 
                                     onkeyup="jsSignUpValidatePassword()" 
                                     name="formSignUpPassword">
-                                    <?php if ($_SESSION['msgid'] == '802' || $_SESSION['msgid'] == '803') { ?>
+                                    <?php if ($_SESSION['msgid'] == '802') { ?>
                                             <div class="invalid-feedback">
                                                 <?php echo (phpShowInputFeedback($_SESSION['msgid'])[1]); ?>
                                             </div>
                                     <?php } ?>                                    
 
                         <input      type="password" 
-                                    class="form-control mt-4" 
+                                    class="form-control mt-4 <?php echo (phpShowInputFeedback($_SESSION['msgid'])[0]); ?>" 
                                     id="formSignUpPasswordConf" 
-                                    placeholder="Confirm your password" 
-                                    required 
-                                    pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$#]).{8,16}" 
+                                    placeholder="Confirm your password"  
                                     onkeyup="jsSignUpValidatePassword()" 
-                                    name="formSignUpPasswordConf">                                  
+                                    name="formSignUpPasswordConf">  
+                                    <?php if ($_SESSION['msgid'] == '803') { ?>
+                                            <div class="invalid-feedback">
+                                                <?php echo (phpShowInputFeedback($_SESSION['msgid'])[1]); ?>
+                                            </div>
+                                    <?php } ?>                                                                      
                     </div>
                     <p id="password_comparison"></p>
-                    <button type="submit" class="btn btn-primary btn-success">Sign Up</button>
+                    <button type="submit" id="formSignUpSubmit" class="btn btn-primary btn-success">Sign Up</button>
                 </form>
             </div>
 
@@ -110,15 +110,158 @@
     <script>
         var jsSignUpPassword = document.getElementById("formSignUpPassword");
         var jsSignUpPasswordConf = document.getElementById("formSignUpPasswordConf");
+        var jsPasswordRegexPattern = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$#]).{8,16}/;
 
-        // check if password matched confirm password
-        function jsSignUpValidatePassword(){
-            if(jsSignUpPassword.value != jsSignUpPasswordConf.value) {
-                jsSignUpPasswordConf.setCustomValidity("Passwords don't match!");
-                document.getElementById("password_comparison").innerHTML = "<div class='alert alert-danger' role='alert'>Passwords don't match!</div>";
+        var jsSignUpEmail = document.getElementById("formSignUpEmail");            
+        var jsEmailRegexPattern = /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/;
+
+        // disabled 'Sign Up' button as default                                    
+        document.getElementById("formSignUpSubmit").disabled = true;
+
+        // replaces green with red box around 'Sign Up' button
+        document.getElementById("formSignUpSubmit").classList.remove('btn-success');
+        document.getElementById("formSignUpSubmit").classList.add('btn-danger');
+
+        // if password and email patterns matched required regex and password == passwordConf, enable the 'Sign Up' button, otherwise, disabled it.
+        function jsSignUpSubmitEnable() {
+            if (jsEmailRegexPattern.test(jsSignUpEmail.value) && jsPasswordRegexPattern.test(jsSignUpPassword.value) && jsSignUpPassword.value == jsSignUpPasswordConf.value) {
+                document.getElementById("formSignUpSubmit").disabled = false;
+                
+                // replaces red with green box around 'Sign Up' button
+                document.getElementById("formSignUpSubmit").classList.remove('btn-danger');
+                document.getElementById("formSignUpSubmit").classList.add('btn-success');
             } else {
-                jsSignUpPasswordConf.setCustomValidity('');
-                document.getElementById("password_comparison").innerHTML = "";
+                document.getElementById("formSignUpSubmit").disabled = true;
+                
+                // replaces green with red box around 'Sign Up' button
+                document.getElementById("formSignUpSubmit").classList.remove('btn-success');
+                document.getElementById("formSignUpSubmit").classList.add('btn-danger');
+            }
+        }
+
+        function jsSignUpValidateEmail() {
+            jsSignUpSubmitEnable();
+
+            // checking email regex
+            if (!jsEmailRegexPattern.test(jsSignUpEmail.value)) {
+
+                // check this id so that error message will not be printed out every time user clicks a key.
+                // we want the message to appear once and stay there unitl email regex is valid
+                if (!document.getElementById("formSignUpEmailInvalidFeedback")) {
+                    // add red box for email input using BS class: 'is-invalid'
+                    jsSignUpEmail.classList.add("is-invalid");                                       
+
+                    // creates a <div> element
+                    var newElement = document.createElement("div");         
+
+                    // sets an attribute id to check for <div> existance later
+                    newElement.setAttribute('id', 'formSignUpEmailInvalidFeedback');                  
+
+                    // BS requires <div class='invalid-feedback'> for $feedback_text, so we add this class to the div above                     
+                    newElement.classList.add("invalid-feedback");  
+
+                    // creates $feedback_text
+                    var newElementContent = document.createTextNode("This is not a valid email address");
+
+                    // adds feedback_text into the <div class="invalid-feedback">This is not a valid email address</div>
+                    newElement.appendChild(newElementContent);          
+
+                    //places this <div> element below email <input>                              
+                    jsSignUpEmail.parentNode.insertBefore(newElement, jsSignUpEmail.nextSibling); 
+                }
+            } else {
+                if (document.getElementById('formSignUpEmailInvalidFeedback')) {
+                    // remove <div id="formSignUpEmailInvalidFeedback"> we created earlier for $feedback_text
+                    document.getElementById("formSignUpEmailInvalidFeedback").parentNode.removeChild(document.getElementById("formSignUpEmailInvalidFeedback"));                    
+                }
+                
+                // replaces red box with green box
+                    // remove red box
+                jsSignUpEmail.classList.remove("is-invalid");                
+                    // add green box
+                jsSignUpEmail.classList.add("is-valid");                     
+            }
+        }
+
+        // checking password regex and whether passwords are matched
+        function jsSignUpValidatePassword() {
+            jsSignUpSubmitEnable();
+
+            // checking password regex
+            if (!jsPasswordRegexPattern.test(jsSignUpPassword.value)) {
+
+                // check this id so that error message will not be printed out every time user clicks a key.
+                // we want the message to appear once and stay there unitl email regex is valid
+                if (!document.getElementById("formSignUpPasswordInvalidFeedback")) {
+                    // add red box for email input using BS class: 'is-invalid'
+                    jsSignUpPassword.classList.add("is-invalid");                                       
+
+                    // creates a <div> element
+                    var newElement = document.createElement("div");  
+
+                    // sets an attribute id to check for <div> existance later
+                    newElement.setAttribute('id', 'formSignUpPasswordInvalidFeedback');   
+
+                    // BS requires <div class='invalid-feedback'> for $feedback_text, so we add this class to the div above                                 
+                    newElement.classList.add("invalid-feedback");     
+
+                    // creates $feedback_text                                 
+                    var newElementContent = document.createTextNode("Password must be between 8 and 16 characters long, with at least one uppercase and lowercase character, one number and one special character (@, *, $ or #).");
+                    
+                    // adds feedback_text into the <div class="invalid-feedback">Password...</div>
+                    newElement.appendChild(newElementContent);      
+
+                    // places this <div> element below password <input> 
+                    jsSignUpPassword.parentNode.insertBefore(newElement, jsSignUpPassword.nextSibling); 
+                }
+            } else if (jsSignUpPassword.value != jsSignUpPasswordConf.value) {
+                if (document.getElementById('formSignUpPasswordInvalidFeedback')) {
+                    // remove <div id="formSignUpPasswordInvalidFeedback"> we created earlier for $feedback_text
+                    document.getElementById("formSignUpPasswordInvalidFeedback").parentNode.removeChild(document.getElementById("formSignUpPasswordInvalidFeedback"));
+                }
+
+                // replaces red box with green box 
+                    // remove red box
+                jsSignUpPassword.classList.remove("is-invalid");  
+                    // add green box
+                jsSignUpPassword.classList.add("is-valid");                   
+                
+                // check this id so that error message will not be printed out every time user clicks a key.
+                // we want the message to appear once and stay there unitl email regex is valid                
+                if (!document.getElementById("formSignUpPasswordConfInvalidFeedback")) {
+                    // add red box for email input using BS class: 'is-invalid'
+                    jsSignUpPasswordConf.classList.add("is-invalid");                  
+
+                    // creates a <div> element
+                    var newElement = document.createElement("div");  
+
+                    // sets an attribute id to check for <div> existance later
+                    newElement.setAttribute('id', 'formSignUpPasswordConfInvalidFeedback');   
+
+                    // BS requires <div class='invalid-feedback'> for $feedback_text, so we add this class to the div above                 
+                    newElement.classList.add("invalid-feedback");    
+
+                    // creates $feedback_text                                     
+                    var newElementContent = document.createTextNode("Passwords don't match!");
+
+                    // adds feedback_text into the <div class="invalid-feedback">Password don't match</div>
+                    newElement.appendChild(newElementContent);                                          
+
+                    // places this <div> element below passwordConf <input> 
+                    jsSignUpPasswordConf.parentNode.insertBefore(newElement, jsSignUpPasswordConf.nextSibling); 
+                }
+            } else {
+                if (document.getElementById('formSignUpPasswordConfInvalidFeedback')) {
+                    // remove <div id="formSignUpPasswordConfInvalidFeedback"> we created earlier for $feedback_text                
+                    document.getElementById("formSignUpPasswordConfInvalidFeedback").parentNode.removeChild(document.getElementById("formSignUpPasswordConfInvalidFeedback"));
+                }
+
+                // replaces red box with green box 
+                    // remove red box
+                jsSignUpPasswordConf.classList.remove("is-invalid");                
+                    // add green box
+                jsSignUpPasswordConf.classList.add("is-valid");                     
+
             }
         }
     </script>
